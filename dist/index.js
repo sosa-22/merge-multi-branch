bf94ce9e5dba831f15aace00c0369c369929b1d8
Samuel Sosa <samuel.sosa@helloiconic.com>
2022-08-15 22:10:39 -0600

    delete target_branch

--- dist/index.js
+++ dist/index.js
@@ -9670,16 +9670,12 @@ const core = __nccwpck_require__(2186);
 async function run() {
   try {
     const token = core.getInput("github_token");
-    const target_branch = core.getInput("target_branch");
     const source_ref = core.getInput("source_ref");
     const commit_message_template = core.getInput("commit_message_template");
     const octokit = github.getOctokit(token);
 
     const repo = github.context.repo;
 
-    let commitMessage = commit_message_template
-      .replace("{source_ref}", source_ref)
-      .replace("{target_branch}", target_branch);
     const { data } = await octokit.rest.repos.listBranches({
       owner: repo.owner,
       repo: repo.repo,
@@ -9688,6 +9684,10 @@ async function run() {
     for (const currentBranch of data) {
       let splitBranch = currentBranch.name.split("-");
       if (splitBranch.pop() === "stable") {
+        let commitMessage = commit_message_template
+          .replace("{source_ref}", source_ref)
+          .replace("{target_branch}", currentBranch.name);
+
         await octokit.rest.repos.merge({
           owner: repo.owner,
           repo: repo.repo,
--- src/action.js
+++ src/action.js
@@ -4,16 +4,12 @@ const core = require("@actions/core");
 async function run() {
   try {
     const token = core.getInput("github_token");
-    const target_branch = core.getInput("target_branch");
     const source_ref = core.getInput("source_ref");
     const commit_message_template = core.getInput("commit_message_template");
     const octokit = github.getOctokit(token);
 
     const repo = github.context.repo;
 
-    let commitMessage = commit_message_template
-      .replace("{source_ref}", source_ref)
-      .replace("{target_branch}", target_branch);
     const { data } = await octokit.rest.repos.listBranches({
       owner: repo.owner,
       repo: repo.repo,
@@ -22,6 +18,10 @@ async function run() {
     for (const currentBranch of data) {
       let splitBranch = currentBranch.name.split("-");
       if (splitBranch.pop() === "stable") {
+        let commitMessage = commit_message_template
+          .replace("{source_ref}", source_ref)
+          .replace("{target_branch}", currentBranch.name);
+
         await octokit.rest.repos.merge({
           owner: repo.owner,
           repo: repo.repo,
